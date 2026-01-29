7761dda7580a025e580e2afcd472e17338286cbd
Jason <13350804+jroland2004@users.noreply.github.com>
2026-01-28 21:37:58 -0500

    Add patient intake formatter

--- dose_calculator.py
+++ dose_calculator.py
@@ -1,6 +1,8 @@
 weight_pounds = float(input("What is the patient's weight in pounds? "))
 dose_by_weight = float(input("How many milligrams are ordered per kilogram? "))
 medication_concentration = float(input("What is concentration available? Provide the number of milligrams availble in each milliliter. "))
+
+#Converts punds to kilograms
 pounds_to_kilo = (weight_pounds / 2.2)
 dose_per_kg = (pounds_to_kilo * dose_by_weight)
 dose_volume = (dose_per_kg / medication_concentration)
---
+++ patient_intake.py
@@ -0,0 +1,6 @@
+
+first_name = input("Enter the Patient's first name: ")
+last_name = input("Enter Patient's last name: ")
+date_of_birth = input("Enter Patient's date of birth: ")
+
+print(f"Patient: {last_name.upper()}, {first_name.upper()}\nDOB: {date_of_birth} ")

e1e6fae0493030cbc73ad1e9b0f8695be64ae82f
Jason <13350804+jroland2004@users.noreply.github.com>
2026-01-31 20:58:12 -0500

    Add patient queue using while and for loops

---
+++ patient_queue.py
@@ -0,0 +1,13 @@
+waiting_room = []
+
+
+while True:
+   name = input("Enter patient name (or 'done' if finished): ")
+   if name == 'done':
+    break
+   waiting_room.append(name)
+
+print(f"Total number of patients waiting: {len(waiting_room)}" )
+
+for name in waiting_room:
+    print(f"Calling: {name}")

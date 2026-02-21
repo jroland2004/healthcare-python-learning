ac28bc35ee368cbf1cafe50a564daa0be3152e62
Jason <13350804+jroland2004@users.noreply.github.com>
2026-02-21 12:52:30 -0500

    Add update_vitals and birthday methods to Patient class

--- patient_with_vitals.py
+++ patient_with_vitals.py
@@ -6,6 +6,12 @@ class Patient:
         self.age = age
         self.vitals = vitals
 
+    def birthday(self):
+        self.age += 1
+
+    def update_vitals(self, new_vitals):
+        self.vitals = new_vitals
+
     def get_report(self):
         report = f"Patient: {self.name}, {self.age} years old\n"
         report += "Vitals Assessment:\n"
@@ -18,4 +24,7 @@ class Patient:
 
 vitals = VitalSigns(45, 125, 82, 98, 92)
 patient = Patient("Jason Roland", 45, vitals)
+
+vitals2 = VitalSigns(88, 145, 92, 99.1, 95)
+patient.update_vitals(vitals2)
 print(patient.get_report())
